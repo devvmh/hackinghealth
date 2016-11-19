8ae61f9ba4e90c6a5bcdb2e6d714e5e8b9d6a7ff
Devin Howard <devin@callysto.com>
2016-11-19 13:05:23 -0500

    fix the three views

---
+++ client/app/bundles/Teacher/containers/ChatroomView.jsx
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+
+export default class ChatroomView extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired, // this is passed from the Rails view
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = { name: this.props.name };
+  }
+
+  updateName(name) {
+    this.setState({ name });
+  }
+
+  render() {
+    const { name } = this.props;
+    return (
+      <div className="container">
+        <h3>
+          Hello, {name}!
+        </h3>
+        <hr />
+        <form className="form-horizontal">
+          <label>
+            Say hello to:
+          </label>
+          <input
+            type="text"
+            value={name}
+            onChange={e => this.updateName(e)}
+          />
+        </form>
+      </div>
+    );
+  }
+}
--- client/app/bundles/Teacher/startup/TeacherView.jsx
+++ client/app/bundles/Teacher/startup/TeacherView.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactOnRails from 'react-on-rails';
 
-import Teacher from '../containers/Teacher';
+import RecordingView from '../containers/RecordingView';
+import ChatroomView from '../containers/ChatroomView';
+import ArchiveView from '../containers/ArchiveView';
 
-const TeacherView = (props) => (
-  <Teacher {...props} />
-);
+const RECORDING_VIEW = 1
+const CHATROOM_VIEW = 2
+const ARCHIVE_VIEW = 3
+
+class TeacherView extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tab: props.initialTab
+    }
+  }
+
+  render() {
+    switch(this.state.tab) {
+      case RECORDING_VIEW:
+        return <RecordingView {...this.props} />
+      case CHATROOM_VIEW:
+        return <ChatroomView {...this.props} />
+      case ARCHIVE_VIEW:
+        return <ArchiveView {...this.props} />
+      default:
+        return <div>Something went wrong</div>
+    }
+  }
+}
 
 ReactOnRails.register({ TeacherView });
